09d1b7b8f028cd9b8b57df16f69dc57787505283
la-liqa <sabina.mammadova.95@gmail.com>
2022-12-22 23:26:46 +0300

    fixed second

--- assets/main.js
+++ assets/main.js
@@ -4,9 +4,12 @@ const left=document.querySelector(".lefttext")
 const right=document.querySelector(".righttext")
 const firstRate=document.querySelector(".firstRate")
 const secondRate=document.querySelector(".secondRate")
+window.addEventListener("offline",()=>{
+    alert("You are not connected to the network.")
+})
 
 buttonsleft.forEach(item=>item.addEventListener("click",(e)=>{
-    const violet=document.querySelectorAll(".base.active")
+    const violet=document.querySelectorAll(".base.right.active")
     violet.forEach(item=>item.classList.remove("active"))
     e.target.classList.add("active")
     const base=e.target.innerText
@@ -14,6 +17,8 @@ buttonsleft.forEach(item=>item.addEventListener("click",(e)=>{
     fetch(`https://api.exchangerate.host/latest?base=${base}&symbols=${symbol.innerText}`)
     .then(res=>res.json())
     .then(res=>{const rate=res.rates[symbol.innerText]; 
+        right.value = +left.value * +rate
+        left.value = +right.value / +rate
         left.addEventListener("keyup",() =>{
             right.value = +left.value * +rate
             
@@ -26,13 +31,12 @@ secondRate.innerText=`1 ${symbol.innerText}=${1/+rate} ${base}`
 }).catch(err=>alert(err))
        
 }))
+document.querySelector(".rub.base.active").click()
 
 buttonsright.forEach(item=>item.addEventListener("click",(e)=>{
     const violet=document.querySelectorAll(".symbol.active")
     violet.forEach(item=>item.classList.remove("active"))
     e.target.classList.add("active")
-    const base=e.target.innerText
-    const symbol=document.querySelector(".symbol.active")
 }))
 
 
