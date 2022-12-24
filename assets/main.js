cb99557a3074b8ce6f3ada64934d05a26f525b30
la-liqa <sabina.mammadova.95@gmail.com>
2022-12-24 23:10:32 +0300

    js fixed last

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-const buttons=document.querySelectorAll("button")
-const buttonsright=document.querySelectorAll(".symbol")
+const buttonLeft=document.querySelectorAll(".base")
+const buttonsRight=document.querySelectorAll(".symbol")
 const left=document.querySelector(".lefttext")
 const right=document.querySelector(".righttext")
 const firstRate=document.querySelector(".firstRate")
@@ -7,7 +7,7 @@ const secondRate=document.querySelector(".secondRate")
 window.addEventListener("offline",()=>{
     alert("You are not connected to the network.")
 })
-buttons.forEach(item=>item.addEventListener("click",(e)=>{
+buttonLeft.forEach(item=>item.addEventListener("click",(e)=>{
     const violet=document.querySelectorAll(".base.active")
     violet.forEach(item=>item.classList.remove("active"))
     e.target.classList.add("active")
@@ -17,23 +17,29 @@ buttons.forEach(item=>item.addEventListener("click",(e)=>{
     .then(res=>res.json())
     .then(res=>{const rate=res.rates[symbol.innerText]; 
         right.value = +left.value * +rate
-        left.value = +right.value / +rate
         left.addEventListener("keyup",() =>{
-            right.value = +left.value * +rate        
+            right.value = +left.value * +rate       
 })
-right.addEventListener("keyup",() =>{
-    left.value = +right.value / +rate
-})
-firstRate.innerText=`1 ${base}=${rate} ${symbol.innerText}`
-secondRate.innerText=`1 ${symbol.innerText}=${1/+rate} ${base}`
+firstRate.innerText=`1 ${base}=${1/rate} ${symbol.innerText}`
 }).catch(err=>alert(err))      
 }))
 document.querySelector(".rub.base.active").click()
-buttonsright.forEach(item=>item.addEventListener("click",(e)=>{
+buttonsRight.forEach(item=>item.addEventListener("click",(e)=>{
     const violets=document.querySelectorAll(".symbol.active")
     violets.forEach(item=>item.classList.remove("active"))
     e.target.classList.add("active")
-}))
+    const symbol=e.target.innerText
+    const base=document.querySelector(".base.active")
+    fetch(`https://api.exchangerate.host/latest?base=${base.innerText}&symbols=${symbol}`)
+    .then(res=>res.json())
+    .then(res=>{const rate=res.rates[symbol]; 
+        left.value = +right.value / +rate
+        right.addEventListener("keyup",() =>{
+            left.value = +right.value / +rate
+        })
+        secondRate.innerText=`1 ${symbol}=${1/rate} ${base.innerText}`
+    })}))
+document.querySelector(".usd.symbol.active").click()
 
 
 
